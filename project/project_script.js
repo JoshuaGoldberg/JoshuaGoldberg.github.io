fddbb3e4e5b6c0bec02d1001ff515a3468707ae9
JoshuaGoldberg <joshua@barrygoldberg.net>
2026-01-02 21:03:02 -0500

    project tags

--- project/project_script.js
+++ project/project_script.js
@@ -1,23 +1,31 @@
 const container = document.getElementById("projects");
 
+const tags = {
+  RESEARCH: "Research",
+  ACADEMIC: "Academic",
+  PERSONAL: "Personal",
+};
+
 const projects = [
   {
     title: "Lean Measure",
-    preview:
-      "An ongoing project to define probabilistic programming in Lean 4.",
+    preview: "An ongoing project to define probabilistic spaces in Lean 4.",
     link: "lean_project.html",
+    tag: tags.RESEARCH,
   },
   {
     title: "Owl Lean",
     preview:
-      "Bidirectional Typechecker for the security language Owl, written in Lean.",
+      "Bidirectional Typechecker for the security language Owl, written in Lean 4.",
     link: "owl_project.html",
+    tag: tags.RESEARCH,
   },
   {
     title: "JustinBot 3000",
     preview:
-      "Agentic Chatbot created to assist TA's within foundational Cybersecurity courses.",
+      "Agentic Chatbot created to assist TA's within foundational Cybersecurity courses. Utilized Retrieval-Augmented Generation (RAG) for targeted responses.",
     link: "cybersecurity_project.html",
+    tag: tags.ACADEMIC,
   },
   {
     title: "Osu!Mania Replay Renderer",
@@ -25,12 +33,14 @@ const projects = [
       "A replay renderer for the rhythm game osu!mania, utilizing LZMA decompression and" +
       " a ground up recreation of the game for recording purposes.",
     link: "osu_project.html",
+    tag: tags.PERSONAL,
   },
   {
     title: "Tap.py",
     preview:
       "A small (and currently incomplete) clicker game written in Python, with the goal of fleshing out complex systems.",
     link: "osu_project.html",
+    tag: tags.PERSONAL,
   },
   {
     title: "Powerdle",
@@ -38,6 +48,7 @@ const projects = [
       "Initially just a joke, this is a basic implementation of an evil Wordle clone. Mainly created to showcase skills learned from" +
       " CS2510, Northeastern's second fundamental computer science course.",
     link: "osu_project.html",
+    tag: tags.PERSONAL,
   },
 ];
 
@@ -48,6 +59,7 @@ projects.forEach((project) => {
             <a href=${project.link}>
                 <span class="works-title">${project.title}</span>
             </a>
+            <span class="project-tag">${project.tag}</span>
           </div>
           <hr style="width:100%; border: 1px solid black; margin-top: 3px">
           <p> ${project.preview} </p>
