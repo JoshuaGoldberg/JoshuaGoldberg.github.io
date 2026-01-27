a384f95fddd619445e173cc7db6bce6575ecef75
JoshuaGoldberg <joshua@barrygoldberg.net>
2026-01-27 15:31:27 -0500

    those who remain when their gcd is 1

--- blog/blog_script.js
+++ blog/blog_script.js
@@ -2,12 +2,20 @@ const container = document.getElementById("blog-posts");
 
 const posts = [
   {
+    title: "Group Theory: CRM",
+    date: "1/27/2025",
+    preview:
+      "You met me at a very chinese time in my academic life. A proof of the Chinese Remainder Theorem from my group theory class.",
+    link: "/blog/blog-posts/group_week_1.html",
+  },
+  {
     title: "Blogs Are Cool",
     date: "1/02/2025",
     preview:
       "A brief history of this websites creation, and perhaps a critique of my own procrastination.",
     link: "/blog/blog-posts/web_blog.html",
   },
+  
 ];
 
 posts.forEach((post) => {
